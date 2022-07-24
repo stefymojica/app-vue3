5673c07ee0bdfccdaa86edcf6b529987cc9af761
Stefany Mojica <stefymojica40@gmail.com>
2022-07-24 17:39:49 -0500

    List Rendering

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ const app = Vue.createApp({
             //url: 'https://www.vuemastery.com/',
             inventory: 100,
             //onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            //sizes: ['S', 'M', 'L', 'XL'],
+            variants: [
+                { id: 2234, color: 'Green'},
+                { id: 2235, color: 'Blue'}
+            ]
         }
     }
 })
