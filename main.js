b438a16669223281d87a4cd603be81b731368b32
Stefany Mojica <stefymojica40@gmail.com>
2022-07-24 16:40:44 -0500

    Attribute Binding

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data: function() {
         return {
             product: 'Socks',
-            description: 'I love socks'
+            description: 'I love socks',
+            image: './assets/images/socks-green.png',
+            url: 'https://www.vuemastery.com/'
         }
     }
 })
