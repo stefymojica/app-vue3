39d0acf116a4c174acf3188fd0291d1bf5ab26bd
Stefany Mojica <stefymojica40@gmail.com>
2022-07-24 19:12:32 -0500

    Event Handling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data: function() {
         return {
+            cart:0,
             product: 'Socks',
             description: 'I love socks',
             image: './assets/images/socks-green.png',
@@ -10,9 +11,22 @@ const app = Vue.createApp({
             details: ['50% cotton', '30% wool', '20% polyester'],
             //sizes: ['S', 'M', 'L', 'XL'],
             variants: [
-                { id: 2234, color: 'Green'},
-                { id: 2235, color: 'Blue'}
+                { id: 2234, color: 'Green', image:'./assets/images/socks-green.png'},
+                { id: 2235, color: 'Blue', image:'./assets/images/socks-blue.png'}
             ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        },
+        removeFromCart() {
+            if (this.cart >= 1) {
+                this.cart -= 1
+            }
         }
     }
 })
